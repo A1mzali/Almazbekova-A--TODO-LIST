d080aacd3ed2aa484e5380914c1102aae452391d
Aliia <112251947+A1mzali@users.noreply.github.com>
2023-03-27 21:30:35 +0300

    ToDo

--- js/main.js
+++ js/main.js
@@ -8,17 +8,41 @@ const emptyList = document.querySelector('#EmtyList')
 //Создаем пустой массив
 let tasks = [];
 
+if(localStorage.getItem('tasks')){
+ tasks = JSON.parse(localStorage.getItem('tasks'))
+
+}
+
+tasks.forEach(function(task){
+    //Формируем сss класс
+  const cssClass = task.done ? 'task-title task-title--done' : 'task-title';  //тернарный оператор
+
+
+  //Формулируем разметку для новой задачи
+   const taskHTML = `<li id='${task.id}' class="list-group-item d-flex justify-content-between task-item">
+            <span class="${cssClass}">${task.text}</span>
+            <div class="task-item__buttons">
+              <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+              </button>
+              <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+              </button>
+            </div>
+          </li>`;
+
+    //Добавляем задачу на страницу
+
+  tasksList.insertAdjacentHTML('beforeend', taskHTML)  
+})
+
+checkEmptyList()
+
 
 //Добавление задачи
 
 form.addEventListener('submit', addTask)
- 
-//удаление задачи
-
 tasksList.addEventListener('click', deleteTask)
-
-//Отмечаем задачу завершенной
-
 tasksList.addEventListener('click', doneTask)
 
 
@@ -39,6 +63,9 @@ function addTask(event){
 
     //добавляем задачу в массив с задачами
     tasks.push(newTask)
+
+    //Сохраняем список задач в хранилище браузера lacalStrorage
+    saveToLOcalStrorage()
 
  //Формируем сss класс
   const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';  //тернарный оператор
@@ -65,6 +92,7 @@ function addTask(event){
      taskInput.value = " ";
      taskInput.focus()  //фокус возвращается к input
 
+     checkEmptyList()
 }
 
 function deleteTask(event) {
@@ -87,8 +115,13 @@ function deleteTask(event) {
         // return task.id !== id;
     })
 
+    //Сохраняем список задач в хранилище браузера lacalStrorage
+    saveToLOcalStrorage()
+
     //удаляем задачу из разметки
-    parentNode.remove()
+    parentNode.remove() 
+
+    checkEmptyList()
 
 }
 
@@ -111,14 +144,31 @@ function doneTask(event) {
 
     task.done = !task.done
 
-    console.log(task);
+    //Сохраняем список задач в хранилище браузера lacalStrorage
+    saveToLOcalStrorage()
 
     const taskTitle = parentNode.querySelector('.task-title');
     taskTitle.classList.toggle('task-title--done');
 
 }
 
+function checkEmptyList() {
+  if(tasks.length === 0) {
+   const emptyListHTML = `<li id="emptyList" class="list-group-item empty-list">
+					<img src="./img/leaf.svg" alt="Empty" width="48" class="mt-3">
+					<div class="empty-list__title">Список дел пуст</div>
+				</li>`;
 
-function saveHTMLtoLS() {
-  localStorage.setItem('tasksHTML', tasksList.innerHTML);
+        tasksList.insertAdjacentHTML('afterbegin', emptyListHTML);
+  }
+
+  if (tasks.length > 0) {
+    const emptyListEl = document.querySelector('#emtyList');
+    emptyListEl ? emptyListEl.remove() : null;
+
+  }
+}
+
+function saveToLOcalStrorage(){
+  localStorage.setItem('tasks',JSON.stringify(tasks))
 }
