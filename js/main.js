5209a535fb078808cbad9548b60767ceadc5bf50
Aliia <112251947+A1mzali@users.noreply.github.com>
2023-03-28 18:40:10 +0300

    main

--- js/main.js
+++ js/main.js
@@ -11,30 +11,12 @@ let tasks = [];
 if(localStorage.getItem('tasks')){
  tasks = JSON.parse(localStorage.getItem('tasks'))
 
+ tasks.forEach(function(task){
+  renderTask(task) ; 
+})
 }
 
-tasks.forEach(function(task){
-    //Формируем сss класс
-  const cssClass = task.done ? 'task-title task-title--done' : 'task-title';  //тернарный оператор
 
-
-  //Формулируем разметку для новой задачи
-   const taskHTML = `<li id='${task.id}' class="list-group-item d-flex justify-content-between task-item">
-            <span class="${cssClass}">${task.text}</span>
-            <div class="task-item__buttons">
-              <button type="button" data-action="done" class="btn-action">
-                <img src="./img/tick.svg" alt="Done" width="18" height="18">
-              </button>
-              <button type="button" data-action="delete" class="btn-action">
-                <img src="./img/cross.svg" alt="Done" width="18" height="18">
-              </button>
-            </div>
-          </li>`;
-
-    //Добавляем задачу на страницу
-
-  tasksList.insertAdjacentHTML('beforeend', taskHTML)  
-})
 
 checkEmptyList()
 
@@ -65,28 +47,11 @@ function addTask(event){
     tasks.push(newTask)
 
     //Сохраняем список задач в хранилище браузера lacalStrorage
-    saveToLOcalStrorage()
+    saveToLOcalStrorage();
 
- //Формируем сss класс
-  const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';  //тернарный оператор
-
-
-   //Формулируем разметку для новой задачи
-    const taskHTML = `<li id='${newTask.id}' class="list-group-item d-flex justify-content-between task-item">
-             <span class="${cssClass}">${newTask.text}</span>
-             <div class="task-item__buttons">
-               <button type="button" data-action="done" class="btn-action">
-                 <img src="./img/tick.svg" alt="Done" width="18" height="18">
-               </button>
-               <button type="button" data-action="delete" class="btn-action">
-                 <img src="./img/cross.svg" alt="Done" width="18" height="18">
-               </button>
-             </div>
-           </li>`;
- 
-     //Добавляем задачу на страницу
- 
-   tasksList.insertAdjacentHTML('beforeend', taskHTML)  
+    // Рендерим задачу на странице
+    renderTask(newTask) ;
+    
    
      // Очищаем INPUT
      taskInput.value = " ";
@@ -171,4 +136,27 @@ function checkEmptyList() {
 
 function saveToLOcalStrorage(){
   localStorage.setItem('tasks',JSON.stringify(tasks))
+}
+
+function renderTask(task) {
+  //Формируем сss класс
+  const cssClass = task.done ? 'task-title task-title--done' : 'task-title';  //тернарный оператор
+
+
+  //Формулируем разметку для новой задачи
+   const taskHTML = `<li id='${task.id}' class="list-group-item d-flex justify-content-between task-item">
+            <span class="${cssClass}">${task.text}</span>
+            <div class="task-item__buttons">
+              <button type="button" data-action="done" class="btn-action">
+                <img src="./img/tick.svg" alt="Done" width="18" height="18">
+              </button>
+              <button type="button" data-action="delete" class="btn-action">
+                <img src="./img/cross.svg" alt="Done" width="18" height="18">
+              </button>
+            </div>
+          </li>`;
+
+    //Добавляем задачу на страницу
+
+  tasksList.insertAdjacentHTML('beforeend', taskHTML)  
 }
